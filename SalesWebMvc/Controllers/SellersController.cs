6562170da05d6161a35ea1619719b54d7594b2d4
PedroMateus26 <pedromateusneto@gmail.com>
2019-07-01 13:08:56 -0300

    Delete seller

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -32,6 +32,7 @@ namespace SalesWebMvc.Controllers
             var viewModel = new SellerFormViewModel { Departments = department };
             return View(viewModel);
         }
+        
 
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -40,5 +41,28 @@ namespace SalesWebMvc.Controllers
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+             _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
+
+ 
 }
